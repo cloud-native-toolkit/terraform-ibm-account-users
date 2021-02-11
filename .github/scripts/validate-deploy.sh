baf13511a73e21accb4f6e89f925d38dafde4771
Sean Sundberg <seansund@us.ibm.com>
2021-02-11 05:54:45 -0600

    Adds ibmcloud login command to validate-deploy.sh

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+ibmcloud login --apikey "${IBMCLOUD_API_KEY}"
+
 if ! ibmcloud account users | grep -q "toolkittest1@ibm.com"; then
   echo "User not added to account: toolkittest1@ibm.com"
+  exit 1
 fi
